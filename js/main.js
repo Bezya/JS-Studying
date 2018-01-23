a002dcdda8c0891988a3c1a1223d6e916ff9d34a
Julia Bezverkha <30863562+Bezya@users.noreply.github.com>
2018-01-23 15:24:30 +0200

    2-d version
    Add one more method to build gallery - by tamplated strings.
    There are already too methods:
    1) by Replace
    2) by tamplated strings

--- js/main.js
+++ js/main.js
@@ -11,24 +11,64 @@
 
     const transformUrl = str => `http://${str}`;
 
-    const getFormattedDate = date => moment(date).format('YYYY/MM/DD hh:mm');
-
     const transformName = name => `${name[0].toUpperCase()}${name.slice(1).toLowerCase()}`;
 
     const transformDescription = str => (str.length > 15) ? `${str.substring(0, 15)}...` : str;
+
+    const getFormattedDate = date => moment(date).format('YYYY/MM/DD hh:mm');
 
     const makeNewArrOfCars = arr => arr.map(item => ({ url, name, description, date } = item));
 
     const modifiedArrOfCars = arr => {
         return arr.map(item => {
             const { url, name, description, date } = item;
-        return {
-            url: transformUrl(url),
-            name: transformName(name),
-            description: transformDescription(description),
-            date: getFormattedDate(date)
+            return {
+                url: transformUrl(url),
+                name: transformName(name),
+                description: transformDescription(description),
+                date: getFormattedDate(date)
             }
         })
+    };
+
+    /*const iteration = (count, arr, template) => {
+        let result = '';
+        for (let i = 0; i < count; i++) {
+            result += template(arr[i]);
+        }
+        return result;
+    };
+
+    let selectNumberOfImages = (count, arr, numberOfBlock) => {
+        switch (count) {
+            case '1':
+                numberOfBlock.innerHTML = iteration(3, arr);
+                break;
+            case '2':
+                numberOfBlock.innerHTML = iteration(6, arr);
+                break;
+            case '0':
+                numberOfBlock.innerHTML = iteration(arr.length, arr);
+                break;
+        }
+    };*/
+
+    const displayCurrentBlock = value => {
+        document.querySelector('.first-group').classList.add("hide");
+        document.querySelector('.second-group').classList.add("hide");
+        document.querySelector('.third-group').classList.add("hide");
+
+        switch (value) {
+            case '1':
+                document.querySelector('.first-group').classList.add("show");
+                break;
+            case '2':
+                document.querySelector('.second-group').classList.add("show");
+                break;
+            case '3':
+                document.querySelector('.third-group').classList.add("show");
+                break;
+        }
     };
 
     function buildGalleryByReplace(count, arr) {
@@ -40,91 +80,82 @@
                                         <div class="text-muted">$date</div>\
                                         </div>\
                                     </div>';
+
         let template = item => {
             return replaceItemTemplate
                 .replace(/\$name/gi, item.name)
                 .replace("$url", item.url)
                 .replace("$description", item.description)
                 .replace("$date", item.date);
-        };
+        }
 
+        // iteration(count, arr, template);
         let iteration = (count, arr) => {
             let result = '';
-            for (let i = 0; i < count; i++){
+            for (let i = 0; i < count; i++) {
                 result += template(arr[i]);
             }
             return result;
         };
 
-        switch (count){
+        //selectNumberOfImages(count, arr, firstBlock);
+        switch (count) {
             case '1':
                 firstBlock.innerHTML = iteration(3, arr);
                 break;
             case '2':
-                firstBlock.innerHTML = iteration(6,arr);
+                firstBlock.innerHTML = iteration(6, arr);
                 break;
             case '0':
                 firstBlock.innerHTML = iteration(arr.length, arr);
                 break;
         }
+
         document.querySelector('.first-group').classList.add("show");
-    }
+    };
 
     function buildGalleryByTemplate(count, arr) {
-        let secondItemTemplate = `<div class="col-sm-3 col-xs-6">\
-                                    <img src="${url}" alt="${name}" class="img-thumbnail">\
-                                    <div class="info-wrapper">\
-                                        <div class="text-muted">${name}</div>\
-                                        <div class="text-muted top-padding">${.description}</div>\
-                                        <div class="text-muted">${date}</div>\
-                                    </div>\
-                                    </div>`;
 
+        // iteration(count, arr, secondItemTemplate);
         let iteration = (count) => {
             let result = '';
-            for (let i = 0; i < count; i++){
+            for (let i = 0; i < count; i++) {
+                let secondItemTemplate = `<div class="col-sm-3 col-xs-6">\
+                                    <img src="${arr[i].url}" alt="${arr[i].name}" class="img-thumbnail">\
+                                    <div class="info-wrapper">\
+                                        <div class="text-muted">${arr[i].name}</div>\
+                                        <div class="text-muted top-padding">${arr[i].description}</div>\
+                                        <div class="text-muted">${arr[i].date}</div>\
+                                    </div>\
+                                    </div>`;
                 result += secondItemTemplate;
             }
             return result;
         };
-        switch (count){
+
+        //selectNumberOfImages(count, arr, secondBlock);
+        switch (count) {
             case '1':
                 secondBlock.innerHTML = iteration(3, arr);
                 break;
             case '2':
-                secondBlock.innerHTML = iteration(6,arr);
+                secondBlock.innerHTML = iteration(6, arr);
                 break;
             case '0':
                 secondBlock.innerHTML = iteration(arr.length, arr);
                 break;
         }
+
         document.querySelector('.second-group').classList.add("show");
-    }
+    };
 
     //function buildGalleryByCreateElement() {
     //}
-    function displayCurrentBlock(value) {
-        document.querySelector('.first-group').classList.add("hide");
-        document.querySelector('.second-group').classList.add("hide");
-        document.querySelector('.third-group').classList.add("hide");
-
-        switch (value){
-            case '1':
-                document.querySelector('.first-group').classList.add("show");
-                break;
-            case '2':
-                document.querySelector('.second-group').classList.add("show");
-                break;
-            case '3':
-                document.querySelector('.third-group').classList.add("show");
-                break;
-        }
-    }
 
     function showGallery() {
         let value = typeGallerySelector.value;
         let count = countSelector.value;
-        switch (value){
+        switch (value) {
             case '1':
                 buildGalleryByReplace(count, modifiedArrOfCars(makeNewArrOfCars(data)));
                 displayCurrentBlock(value);
@@ -134,14 +165,11 @@
                 displayCurrentBlock(value);
                 break;
             case '3':
-                console.log(value);
+                //buildGalleryByCreateElement(count, modifiedArrOfCars(makeNewArrOfCars(data)));
+                //displayCurrentBlock(value);
                 break;
         }
-    }
+    };
+
     btn.addEventListener("click", showGallery);
 })();
-
-
-
-
-
