298c399af1933f97e310874a0dd00c6ba779d352
Bezya <bezverkhaya@gmail.com>
2018-02-27 17:23:25 +0200

    Commits

--- HW_Session_25_LoginForm/js/main.js
+++ HW_Session_25_LoginForm/js/main.js
@@ -1,117 +1,111 @@
-'user strict';
-
+'user strict'
 const domElements = {
     alert: document.querySelector('.alert'),
-    loginForm1: document.querySelector('.form-signin'),
-    loginForm2: document.querySelector('.form-signin2'),
-    inputEmail: document.querySelector('#inputEmail'),
-    inputPassword: document.querySelector('#inputPassword'),
+    loginForm: document.querySelector('.form-signin'),
+    userInfoForm: document.querySelector('.form-signin2'),
+    email: document.querySelector('#inputEmail'),
+    password: document.querySelector('#inputPassword'),
+    userEmail: document.querySelector('#inputEmail2'),
+    userPassword: document.querySelector('#inputPassword2'),
     checkBoxRememberMe: document.querySelector('#remember-me'),
     btnSingIn: document.querySelector('#btn-sign'),
     btnShowPass: document.querySelector('#btn-showPass'),
     btnGoBack: document.querySelector('#btn-goBack'),
 };
 
-let errorMsg = ['Заполните поля логин и пароль',
-                'Логин введен неправильно! Разрешены только латинские буквы,цифры, следуйте шаблону!',
-                'Введены недопустимые символы! Разрешены только латинские буквы и цифры!',
-                'Пароль слишком короткий! Пароль должен быть не менее 6 символов!',
-                'Неправильные логин или пароль'
-];
-
-let hidePass = 'Скрыть пароль';
-
-let printResult = (elem, input) => elem.innerHTML = input;
-
-function LoginForm(login, password, alert, errorMsg, remember) {
-    this.domLogin = login.value;
-    this.domPass = password.value;
+function LoginForm(login, password, alert, remember, loginForm, userInfoForm, userLogin, userPassword) {
+    this.domLogin = login;
+    this.domPass = password;
     this.alert = alert;
-    this.errorMsg = errorMsg;
-    this.remember = remember.value;
+    this.checkBox = remember;
+    this.domForm1 = loginForm;
+    this.domForm2 = userInfoForm;
+    this.domUserLogin = userLogin;
+    this.domUserPassword = userPassword;
 
-    let isAnyData = () => {
-        if (this.domLogin && this.domPass) {
-            return true;
-        } else {
-            alert(this.errorMsg[0]);
-        }
-    };
+    let errorMsg = ['Заполните поля логин и пароль',
+        'Логин введен неправильно! Разрешены только латинские буквы,цифры, следуйте шаблону!',
+        'Паоль введен неправильно! Разрешены только латинские буквы и цифры!',
+        'Пароль слишком короткий! Пароль должен быть не менее 6 символов!',
+        'Неправильные логин или пароль'
+    ];
+
+    let isAnyData = () => this.domLogin.value && this.domPass.value ? true : alert(errorMsg[0]);
 
     let isValidMail = () => {
-        let checkedInputData = this.domLogin.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
-        if (!!checkedInputData) {
-            return true;
-        } else {
-            alert(this.errorMsg[1]);
-        }
+        let checkedInputData = this.domLogin.value.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+        !!checkedInputData ? true : alert(errorMsg[1]);
     };
 
     let isValidPass = () => {
-        let pass = this.domPass;
+        let pass = this.domPass.value;
         let checkedInputData = pass.match(/^[a-zA-Z0-9]+$/);
         let len = pass.length >= 6 && pass.length <= 20;
         if (!checkedInputData) {
-            alert(this.errorMsg[2]);
+            alert(errorMsg[2]);
             return false;
-        }
-        if (!len) {
-            alert(this.errorMsg[3]);
+        } else if (!len) {
+            alert(errorMsg[3]);
             return false;
+        } else {
+            return true;
         }
-        return true;
+
     };
 
-    this.setLogAndPass = function () {
-        if (!isAnyData()) {
-            return;
-        }
-        if (!isValidMail()) {
-            return;
-        }
-        if (!isValidPass()) {
-            return;
-        }
-        if (this.remember) {
-            localStorage.setItem('login', this.domLogin);
-            localStorage.setItem('password', this.domPass);
-        }
-    };
-}
-
-function showPassword(e){
-    let showedPass = service.showElement()
-
-}
-
-function goBack(e){
-
-}
-
-function alertHandler(msg) {
-    let elem = domElements.alert;
-        if (msg) {
-            setTimeout(function () {
-                elem.classList.remove("show");
-                elem.classList.add("hide");
+    let alertHandler = () =>{
+        if (errorMsg) {
+            setTimeout(() =>{
+                this.alert.classList.remove("show");
+                this.alertclassList.add("hide");
             }, 3000);
-            elem.classList.remove("hide");
-            printResult(elem, msg);
-            elem.classList.add("show");
+            this.alert.classList.remove("hide");
+            this.alert.innerHTML = errorMsg;
+            this.alert.classList.add("show");
         }
+    };
+
+    this.setLogAndPass = function() {
+        if (!isAnyData()) {return;}
+        if (!isValidMail()) {return;}
+        if (!isValidPass()) {return;}
+        if (this.checkBox.value) {
+            localStorage.setItem('login', this.domLogin.value);
+            localStorage.setItem('password', this.domPass.value);
+            this.domUserLogin.value = localStorage.login;
+            this.domUserPassword.value = localStorage.password;
+        }
+    };
+
+    this.initComponents = function() {
+    }
 }
 
-function btnSingIn(e) {
-    e.preventDefault();
-    loginForm.setLogAndPass();
+let loginForm = new LoginForm(domElements.email, domElements.password, alertHandler, domElements.checkBoxRememberMe);
+
+let changeBtnName = () =>{
+    let elem = domElements.btnShowPass;
+};
+
+function showPassword(){
+    let elem = domElements.userPassword;
+    elem.type == 'password' ?  elem.type = 'text' : elem.type = 'password';
+}
+
+let showUserInfoForm = () => {
+    userLoginForm.classList.toggle('display-hide');
+    userInfoForm.classList.toggle('display-hide');
+};
+
+function goBack(){
+    let elem  = domElements.btnGoBack;
 }
 
 function initListeners (){
-    domElements.btnSingIn.addEventListener("click", btnSingIn);
+    domElements.btnSingIn.addEventListener("click", loginForm.setLogAndPass);
+    domElements.btnSingIn.addEventListener("click", showUserInfoForm);
     domElements.btnShowPass.addEventListener("click", showPassword);
     domElements.btnGoBack.addEventListener("click", goBack);
 }
 
-let loginForm = new LoginForm(domElements.inputEmail, domElements.inputPassword, alertHandler, errorMsg, domElements.checkBoxRememberMe);
 initListeners();
-
