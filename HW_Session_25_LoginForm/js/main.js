ec123d398d5969927ba27e24ac4910625c42fc41
Bezya <bezverkhaya@gmail.com>
2018-03-05 23:07:43 +0200

    Commits: 1)finished LoginForm through the self-executing function

--- HW_Session_25_LoginForm/js/main.js
+++ HW_Session_25_LoginForm/js/main.js
@@ -1,117 +1,87 @@
-'user strict'
+'user strict';
+(function(){
+const login = document.querySelector('#inputEmail'),
+    password = document.querySelector('#inputPassword'),
+    userLogin = document.querySelector('#userLogin'),
+    userPassword = document.querySelector('#userPassword'),
+    loginForm = document.querySelector('.form-signin'),
+    userInfoForm = document.querySelector('.form-signin2'),
+    alert = document.querySelector('.alert'),
+    checkBoxRememberMe = document.querySelector('#remember-me'),
+    btnSingIn = document.querySelector('#btn-sign'),
+    btnShowPass = document.querySelector('#btn-showPass'),
+    btnGoBack = document.querySelector('#btn-goBack');
 
-const domElements = {
-    alert: document.querySelector('.alert'),
-    loginForm: document.querySelector('.form-signin'),
-    userInfoForm: document.querySelector('.form-signin2'),
-    email: document.querySelector('#inputEmail'),
-    password: document.querySelector('#inputPassword'),
-    userEmail: document.querySelector('#inputEmail2'),
-    userPassword: document.querySelector('#inputPassword2'),
-    checkBoxRememberMe: document.querySelector('#remember-me'),
-    btnSingIn: document.querySelector('#btn-sign'),
-    btnShowPass: document.querySelector('#btn-showPass'),
-    btnGoBack: document.querySelector('#btn-goBack'),
-};
-
-function LoginForm(login, password, alert, remember, loginForm, userInfoForm, userLogin, userPassword) {
-    this.domLogin = login;
-    this.domPass = password;
-    this.alert = alert;
-    this.checkBox = remember;
-    this.domForm1 = loginForm;
-    this.domForm2 = userInfoForm;
-    this.domUserLogin = userLogin;
-    this.domUserPassword = userPassword;
-
-    let errorMsg = ['Заполните поля логин и пароль',
-        'Логин введен неправильно! Разрешены только латинские буквы,цифры, следуйте шаблону!',
-        'Паоль введен неправильно! Разрешены только латинские буквы и цифры!',
-        'Пароль слишком короткий! Пароль должен быть не менее 6 символов!',
-        'Неправильные логин или пароль'
-    ];
-    let regExMail = /^([\w\._]+)@\1\.([a-z]{2,6}\.?)$/i;
-    let regExPass = /^[a-zA-Z0-9]+$/;
-
-    let isAnyData = () => !this.domLogin.value && !this.domPass.value ? alert(errorMsg[0]): true;
-
-    let isValidMail = () => {
-        let checkedInputData = this.domLogin.value.match(regExMail);
-        !!checkedInputData ? true : alert(errorMsg[1]);
+    let setLogAndPass = {
+            login: 'bezverkhaya@gmail.com',
+            password: '123123123',
+            toLStorage: function(){
+                localStorage.setItem('login', this.login);
+                localStorage.setItem('password', this.password);
+            }
     };
 
-    let isValidPass = () => {
-        let pass = this.domPass.value;
-        let checkedInputData = pass.match(regExPass);
-        let len = pass.length >= 6 && pass.length <= 20;
-        if (!checkedInputData) {
-            alert(errorMsg[2]);
-            return false;
-        } else if (!len) {
-            alert(errorMsg[3]);
-            return false;
-        } else {
-            return true;
-        }
-
-    };
-
-    this.setLogAndPass = function() {
-        if (!isAnyData()) {return;}
-        if (!isValidMail()) {return;}
-        if (!isValidPass()) {return;}
-        if (this.checkBox.value) {
-            localStorage.setItem('login', this.domLogin.value);
-            localStorage.setItem('password', this.domPass.value);
-            //this.domUserLogin.value = localStorage.login;
-            //this.domUserPassword.value = localStorage.password;
+    let fillUserInfoForm = () => {
+        console.log(checkBoxRememberMe.checked);
+        if (checkBoxRememberMe.checked) {
+            userLogin.value = localStorage.getItem('login');
+            userPassword.value = localStorage.getItem('password');
         }
     };
 
-    this.initComponents = function() {
-    }
-}
+    let showUserInfoForm = () => {
+        fillUserInfoForm();
+        loginService.hideElement(loginForm);
+        loginService.showElement(userInfoForm);
 
-let alertHandler = (msg) =>{
-    let elem = domElements.alert;
-    if (msg) {
-        setTimeout(() =>{
-            elem.classList.remove("show");
-            elem.alertclassList.add("hide");
-        }, 3000);
-        elem.classList.remove("hide");
-        elem.innerHTML = msg;
-        elem.classList.add("show");
-    }
-};
+    };
 
-let loginForm = new LoginForm(domElements.email, domElements.password, alertHandler, domElements.checkBoxRememberMe);
+    let showLoginForm = () => {
+        loginService.hideElement(userInfoForm);
+        loginService.showElement(loginForm)
+    };
 
-function pressBtnSingIn(e) {
-    e.preventDefault();
-    loginForm.setLogAndPass();
-}
+    let setBtnName = () => {
+        if (userPassword.type === 'password'){
+            btnShowPass.innerHTML = 'Показать пароль';
+        }else if(userPassword.type === 'text'){
+            btnShowPass.innerHTML = 'Скрыть пароль';
+        }
+    };
 
+    let showPassword = () => {
+        userPassword.type == 'password' ?  userPassword.type = 'text' : userPassword.type = 'password';
+        setBtnName();
+    };
 
-let changeBtnName = () =>{
-    let elem = domElements.btnShowPass;
-};
+    let alertHandler = (alert) => {
+        let msg = loginService.errorMsg()
+        if (msg){
+            loginService.showElement(alert);
+            alert.innerHTML = msg;
+        }
+    };
 
-function showPassword(){
-    let elem = domElements.userPassword;
-    elem.type == 'password' ?  elem.type = 'text' : elem.type = 'password';
-}
+    let initComponents = () =>{
+        if (!loginService.validation(login.value, password.value)) {
+            alertHandler(alert);
+        }else{
+            loginService.hideElement(alert);
+            showUserInfoForm();
+        }
+    };
 
+    let singIn = e => {
+        e.preventDefault();
+        initComponents();
+    };
 
-function goBack(){
-    let elem  = domElements.btnGoBack;
-}
+    let initListeners = () => {
+        btnSingIn.addEventListener("click", singIn);
+        btnShowPass.addEventListener("click", showPassword);
+        btnGoBack.addEventListener("click", showLoginForm);
+    };
 
-function initListeners (){
-    domElements.btnSingIn.addEventListener("click", pressBtnSingIn);
-    //domElements.btnSingIn.addEventListener("click", showUserInfoForm);
-    domElements.btnShowPass.addEventListener("click", showPassword);
-    domElements.btnGoBack.addEventListener("click", goBack);
-}
-
-initListeners();
+    setLogAndPass.toLStorage();
+    initListeners();
+}());
---
+++ HW_Session_25_LoginForm/js/service.js
@@ -0,0 +1,44 @@
+let loginService = (function () {
+
+    let patternMail = /^\w+@\w+\.\w{2,4}$/i;
+    let patternPass = /^[a-zA-Z0-9]+$/;
+    let error = null;
+
+    let getErrorMsg = () => error;
+
+    let setErrorMsg = (i) =>{
+        console.log(i);
+        let arr = ['Заполните поля логин и пароль',
+            'Логин введен неправильно! Разрешены только латинские буквы, цифры, следуйте шаблону!',
+            'Пароль введен неправильно! Разрешены только латинские буквы и цифры!',
+            'Пароль слишком короткий! Пароль должен быть не менее 6 символов!',
+            'Неправильные логин или пароль'];
+        error =  arr[i];
+    };
+
+    let isAnyData = (login, pass) => login && pass ? true : setErrorMsg(0);
+
+    let isValidMailAndPass = (login, pass) => {
+        return (login.match(patternMail) ? true : setErrorMsg(1)) &&
+            (pass.match(patternPass) ? true : setErrorMsg(2)) &&
+            (pass.length >= 6 && pass.length <= 20 ? true : setErrorMsg(3));
+    };
+
+    let compareLogAndPass = (login, pass) => localStorage.getItem('login') == login && localStorage.getItem('password') == pass ?  true : setErrorMsg(4);
+
+    let checkValidation = (login, pass) => {
+        return isAnyData(login, pass) &&
+        isValidMailAndPass(login, pass) &&
+        compareLogAndPass(login, pass)
+    };
+
+    let hideElement = element => element.classList.add("hide");
+    let showElement = element => element.classList.remove("hide");
+
+    return {
+        validation: checkValidation,
+        hideElement: hideElement,
+        showElement: showElement,
+        errorMsg: getErrorMsg
+    }
+})();
