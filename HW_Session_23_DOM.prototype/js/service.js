30c795ed6539dc59ac4ca565db854628e5770d33
Bezya <bezverkhaya@gmail.com>
2018-03-11 16:49:26 +0200

    Version with prototype

---
+++ HW_Session_23_DOM.prototype/js/service.js
@@ -0,0 +1,56 @@
+var galleryService = (function() {
+    const transformUrl = str => (str.indexOf('http://') !== 0) ? `http://${str}` : `${str}`;
+
+    const transformName = name => `${name[0].toUpperCase()}${name.slice(1).toLowerCase()}`;
+
+    const transformDescription = str => (str.length > 15) ? `${str.substring(0, 15)}...` : str;
+
+    const getFormattedDate = date => moment(date).format('YYYY/MM/DD');
+
+    let templateES6 = item => {
+        return `<div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" 
+                            data-src="holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail" 
+                            alt="${transformName(item.name)}" 
+                            src="${transformUrl(item.url)}" data-holder-rendered="true" 
+                            style="height: 225px; width: 100%; display: block;">
+                        <div class="card-body">
+                            <div class="text-muted">${transformName(item.name)}</div>\
+                            <p class="card-text">${transformDescription(item.description)}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn btn-danger" data-id="${item.id}">Удалить</div>
+                                <small class="text-muted">${getFormattedDate(item.date)}</small>
+                            </div>
+                        </div>
+                    </div>
+		        </div>
+	            </div></div>`;
+    };
+
+    let sortNameAsc = (a, b) => a.name > b.name ? 1 : -1;
+    let sortNameDesc = (a, b) => a.name < b.name ? 1 : -1;
+    let sortDateAsc = (a, b) => new Date(a.date) < new Date(b.date) ? 1 : -1;
+    let sortDateDesc = (a, b) => new Date(a.date) > new Date(b.date) ? 1 : -1;
+
+    let removePNode = (e, element) => {
+        let target = e.target;
+        while (target !== element) {
+            if (target.parentNode === element) {
+                element.removeChild(target);
+                break;
+            } else {
+                target = target.parentNode
+            }
+        }
+    };
+
+    return {
+        getGalleryItemHTML: templateES6,
+        sortNameAsc: sortNameAsc,
+        sortNameDesc: sortNameDesc,
+        sortDateAsc: sortDateAsc,
+        sortDateDesc: sortDateDesc,
+        removeParentNode: removePNode
+    }
+}());
