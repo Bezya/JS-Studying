2e87f7c93e0cc446a00cefbe4adac3906d17c68d
Julia Bezverkha <30863562+Bezya@users.noreply.github.com>
2018-04-03 12:57:36 +0300

    Update login.js

--- HW_Session_33_Ajax/public/js/login.js
+++ HW_Session_33_Ajax/public/js/login.js
@@ -1,145 +1,231 @@
-let service = new Service();
+let galleryService = new GalleryService();
 
-let loginService = new LoginService();
+class BaseGallery {
+    constructor(editCallBack) {
+        this.btnAdd = document.querySelector("#add-photo");
+        this.counter = document.querySelector("#js-count");
+        this.dateDropdown = document.querySelector("#dropdown-date");
+        this.nameDropdown = document.querySelector("#dropdown-name");
+        this.gallery = document.querySelector(".js-gallery");
+        this.createUrl = document.querySelector("#createUrl");
+        this.createName = document.querySelector("#createName");
+        this.createDescription = document.querySelector("#createDescription");
+        this.editCallBack = editCallBack;
 
-class LoginForm {
-    constructor(userInfoModule) {
-        this.userInfo = userInfoModule;
-        this.gallery = new BaseGallery(this.editCallBack.bind(this));
-        //this.observer = observer;
-
-        this.inputLogin = document.querySelector('#inputEmail');
-        this.inutPassword = document.querySelector('#inputPassword');
-        this.loginForm = document.querySelector('.form-signin');
-        this.alert = document.querySelector('.alert');
-        this.btnSingIn = document.querySelector('#btn-sign');
-        this.createAndUpdateForm = document.querySelector(".create-form");
-        this.btnCreateAndUpdate = document.querySelector("#btn-create");
-        this.btnCreateItem = document.querySelector("#create-item");
-
-        this.navBlock = document.querySelector('.nav-block');
-        this.navGallary = document.querySelector('#nav-gallery');
-        this.navAboutUser = document.querySelector('#nav-user');
-        this.btnExit = document.querySelector('#btn-exit');
-
-        this.userInfoForm = document.querySelector('.userInfo-form');
-        this.galleryForm = document.querySelector('.gallery-form');
+        this.imgData = null;
+        this.showedImgData = [];
+        this.ready = false;
+        this.url = "http://localhost:3000/cars";
+    }
+    isReady() {
+        return this.ready;
     }
 
     initComponent() {
-        this.checkPage();
-        this.initListeners();
+        let dataResponse = this.dataResponse();
+        if (dataResponse) {
+            dataResponse.then(data => {
+                this.saveData(data);
+                this.buildGallery();
+                this.ready = true;
+            })
+        }
+    }
+
+    dataResponse() {
+        return fetch(this.url).then(responce => responce.json())
+    }
+
+    saveData(data) {
+        this.imgData = data;
     }
 
     initListeners() {
-        this.btnSingIn.addEventListener("click", this.singIn.bind(this));
-        this.navGallary.addEventListener("click", this.showGalleryAndNav.bind(this));
-        this.navAboutUser.addEventListener("click", this.showAndInitUserInfoForm.bind(this));
-        this.btnExit.addEventListener("click", this.logOut.bind(this));
-        this.btnCreateItem.addEventListener("click", this.showCreateAndUpdateFom.bind(this));
-        this.btnCreateAndUpdate.addEventListener("click", this.createGalleryItem.bind(this));
-        //this.btnCreateAndUpdate.addEventListener("click", this.updateGalleryItem.bind(this));
+        //this.btnAdd.addEventListener("click", this.addOneImg.bind(this));
+        //this.nameDropdown.addEventListener("click", this.sortingHandler.bind(this, 'Name'));
+        //this.dateDropdown.addEventListener("click", this.sortingHandler.bind(this, 'Date'));
+        this.gallery.addEventListener("click", this.imgEdit.bind(this));
+        this.gallery.addEventListener("click", this.deleteItem.bind(this));
     }
 
-    initGallery(data) {
-        if(!this.gallery.isReady()) {
-            this.gallery.initComponent(data);
-            this.gallery.initListeners();
-        }
-    }
-
-    createGalleryItem(e){
-        this.gallery.createItem(e).then(()=>{
-            this.showGalleryAndNav();
+    bodyRequest() {
+        return JSON.stringify({
+            url: this.createUrl.value,
+            name: this.createName.value,
+            description: this.createDescription.value,
+            date: Date.now()
         })
     }
 
-    editCallBack(){
-        this.showCreateAndUpdateFom();
+    requestMethod(method) {
+        let requestMethod = {
+            headers: {
+                'Content-type': 'application/json; charset=utf-8'
+            },
+            method: method,
+            body: this.bodyRequest()
+        };
+        return requestMethod;
     }
 
-    /*updateGalleryItem(e){
-        this.gallery.updateItem(e).then(()=>{
-            this.showGalleryAndNav();
+    /*fetch(url, method){
+        return fetch(url, method).then(response =>{
+            if (response.status == 201){
+                return response.json();
+            }
+            throw new Error(response.status);
         })
     }*/
 
-    showGalleryAndNav() {
-        service.hideElement(this.loginForm);
-        service.hideElement(this.userInfoForm);
-        service.hideElement(this.createAndUpdateForm);
-        service.showElement(this.galleryForm);
-        service.showElement(this.navBlock);
+    createItem(e) {
+        e.preventDefault();
+        let requestMethod = this.requestMethod("post");
+        return fetch(this.url, requestMethod).then(response => {
+                if (response.status == 201) {
+                    return response.json();
+                }
+                throw new Error(response.status);
+            })
+            .then(() => this.initComponent())
+            .catch(e => e);
     }
 
-    showAndInitUserInfoForm() {
-        service.hideElement(this.galleryForm);
-        service.showElement(this.userInfoForm);
-        this.userInfo.initComponent();
+    deleteItem(e) {
+        e.preventDefault();
+        let id = e.target.getAttribute("data-id");
+        let options = {
+            headers: {
+                'Content-type': 'application/json; charset=utf-8'
+            },
+            method: 'delete',
+        };
+        fetch("http://localhost:3000/cars" + id, options).then(response => response.json())
+            .then(() => this.initComponent())
+            .catch(e => e);
     }
 
-    showCreateAndUpdateFom(){
-        service.hideElement(this.userInfoForm);
-        service.hideElement(this.galleryForm);
-        service.showElement(this.createAndUpdateForm);
+    updateItem(e) {
+        e.preventDefault();
+        let id = e.target.getAttribute("data-id");
+        let requestMethod = this.requestMethod("put");
+
+        return fetch("http://localhost:3000/cars" + id, requestMethod).then(response => {
+                if (response.status == 201) {
+                    return response.json();
+                }
+                throw new Error("Error");
+            })
+            .then(() => this.initComponent())
+            .catch(e => e);
     }
 
-    logOut() {
-        localStorage.removeItem('login');
-        localStorage.removeItem('password');
-        service.hideElement(this.galleryForm);
-        service.hideElement(this.navBlock);
-        service.hideElement(this.userInfoForm);
-        service.hideElement(this.createAndUpdateForm);
-        service.showElement(this.loginForm);
+    fillFields(item) {
+        this.createUrl.value = item.url;
+        this.createName.value = item.name;
+        this.createDescription.value = item.description;
     }
 
-    alertHandler(alert) {
-        let msg = loginService.getErrorMsg();
-        if (msg) {
-            service.showElement(alert);
-            alert.innerHTML = msg;
+    imgEdit(e) {
+        e.preventDefault();
+        if (e.target.classList.contains('edit')) {
+            let element = e.target;
+            while (!element.classList.contains('gallery-item')) {
+                element = element.parentNode;
+            }
+            let id = element.getAttribute('data-id');
+            let imgForUpdate = this.imgData.find((item) => item.id == id);
+            this.fillFields(imgForUpdate);
+            this.editCallBack();
         }
     }
 
-    logAndPassValidation() {
-        let val = loginService.validation(this.inputLogin.value, this.inutPassword.value);
-            if(val){
-                val.then(res => {
-                    if (res) {
-                        service.hideElement(this.alert);
-                        localStorage.setItem('login', this.inputLogin.value);
-                        localStorage.setItem('password', this.inutPassword.value);
-                        this.showGalleryAndNav();
-                        this.initGallery();
-                    } else {
-                        this.alertHandler(this.alert)
-                    }
-                })
-            } else {
-                    this.alertHandler(this.alert)
-            }
+    buildGallery() {
+        let result = '';
+        this.imgData.forEach(item => {
+            result += galleryService.galleryTemplate(item);
+        });
+        this.gallery.innerHTML = result;
     }
 
-    singIn(e) {
+    /*getShowedImgData() {
+        let localShowedImgData = localStorage.getItem('showedImgData');
+        if (localShowedImgData) {
+            this.showedImgData = JSON.parse(localShowedImgData);
+            this.restoreImg();
+        }
+    }
+
+    setNumberOfImg() {
+        this.counter.innerHTML = this.imgData.length;
+    }
+
+    checkImgDataEnd(arr) {
+        if (arr.length === 0) {
+            this.btnAdd.classList.add("disabled");
+            $('#jsModal').modal('show');
+            return true
+        }
+        return false;
+    }
+
+    restoreImg(sortingFunction) {
+        this.gallery.innerHTML = '';
+        this.showedImgData.sort(sortingFunction);
+        this.showedImgData.forEach((item) => this.gallery.innerHTML += galleryService.galleryTemplate(item));
+        this.setNumberOfImg();
+    }
+
+    updateLocalImgData() {
+        localStorage.setItem('showedImgData', JSON.stringify(this.showedImgData));
+    }
+
+    addOneImg() {
+        if (this.checkImgDataEnd(this.imgData)) return;
+        this.showedImgData.push(this.imgData.shift());
+        this.gallery.innerHTML += galleryService.galleryTemplate(this.showedImgData[this.showedImgData.length - 1]);
+        this.setNumberOfImg();
+        this.updateLocalImgData();
+    }
+
+    imgDelete(e) {
         e.preventDefault();
-        this.logAndPassValidation();
+        if (e.target.classList.contains('btn-danger')) {
+            let id = e.target.getAttribute('data-id');
+            let imgForDelete = this.showedImgData.find((item) => item.id == id);
+            this.showedImgData = this.showedImgData.filter(item => item.id !== imgForDelete.id);
+            this.imgData.push(imgForDelete);
+            galleryService.removePNode(e, this.gallery);
+        }
+        this.btnAdd.classList.remove("disabled");
+        this.setNumberOfImg();
+        this.updateLocalImgData();
+    }*/
+
+    sortingHandler(type, event) {
+        event.preventDefault();
+        event.currentTarget.querySelector("button").innerHTML = event.target.innerText;
+        let sortingType = event.target.getAttribute("sorting-type");
+        sortingType && this.applySortingMethod(sortingType);
+        localStorage.setItem(`sortingType${type}`, sortingType);
     }
 
-    checkSession() {
-        let log = localStorage.getItem('login');
-        let pass = localStorage.getItem('password');
-        return !!log && !!pass
+    applySortingMethod(sortingType) {
+        this.gallery.innerHTML = '';
+        this.restoreImg(galleryService.sortingConfig[sortingType]);
     }
 
-    checkPage() {
-        if (this.checkSession()) {
-            service.showElement(this.galleryForm);
-            service.hideElement(this.loginForm);
-            service.showElement(this.navBlock);
-            this.initGallery();
-        } else {
-            service.showElement(this.loginForm);
+    checkSorting() {
+        let typeName = localStorage.getItem('sortingTypeName');
+        let typeDate = localStorage.getItem('sortingTypeDate');
+        if (typeName) {
+            this.nameDropdown.querySelector("button").innerHTML = typeName === 'A' ?
+                'Вперед: от А до Я' : 'Назад: от Я до А';
+            this.applySortingMethod(typeName);
+        }
+        if (typeDate) {
+            this.dateDropdown.querySelector("button").innerHTML = typeName === 'New' ?
+                'Сначала новые' : 'Сначала старые';
+            this.applySortingMethod(typeName);
         }
     }
 }
